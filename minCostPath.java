fee1542848ec2d60546b27063e4b4fc7f6422b8e
krishnaraj <krishnaraj@chargebee.com>
2020-05-19 23:36:09 +0530

    mcp reconstruct

--- minCostPath.java
+++ minCostPath.java
@@ -7,6 +7,7 @@ public class minCostPath {
                 { 29, 43, 22, 50, 10 } };
 
         System.out.println(findMcp(g));
+        System.out.println(findMcpR(g));
     }
 
     static int findMcp(int[][] g) {
@@ -35,4 +36,53 @@ public class minCostPath {
 
         return dp[g.length - 1][g[0].length - 1];
     }
+
+    static int findMcpR(int[][] g) {
+        int[][] dp = new int[g.length][g[0].length];
+        boolean[][] dir = new boolean[g.length][g[0].length];
+
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                int m = Integer.MAX_VALUE;
+                boolean d = true;
+                if (i > 0) {
+                    m = dp[i - 1][j];
+                    d = true;
+                }
+                if (j > 0 && dp[i][j - 1] < m) {
+                    m = dp[i][j - 1];
+                    d = false;
+                }
+                dp[i][j] = g[i][j] + m;
+                dir[i][j] = d;
+            }
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        for (int i = 0; i < dir.length; i++) {
+            System.out.println(Arrays.toString(dir[i]));
+        }
+
+        int i = dp.length - 1;
+        int j = dp[0].length -1;
+        while(i != 0 || j != 0) {
+            System.out.println(dir[i][j] ? "UP" : "DOWN");
+            if(dir[i][j]) {
+                i = i-1;
+            } else {
+                j = j-1;
+            }
+        }
+
+        return dp[g.length - 1][g[0].length - 1];
+    }
+
+
 }
